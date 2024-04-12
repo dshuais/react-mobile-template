c2f8257e955b70572d8b8212c59bf5c996f7fabd
dshuais <1137896420@qq.com>
2024-04-12 18:32:56 +0800

    🧪 test: 研究全局监听路由方案

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
+<!--
+ * @Author: dushuai
+ * @Date: 2024-04-12 11:02:43
+ * @LastEditors: dushuai
+ * @LastEditTime: 2024-04-12 18:14:57
+ * @description: readme
+-->
+
 # React-Mboile-Template
 
 一个自用的 React 移动端 H5 项目模板，主要为了减少每次项目开发前的重复操作。主要采用 Vite + React + TypeScript + SWC + Tailwind css + Valtio + Ant Design Mobile 等常用技术栈和常用库构建。
@@ -10,6 +18,7 @@
 - [x] 区分环境变量
 - [x] Valtio 状态管理仓库
 - [x] Svg To Component
+- [x] 弹窗控制理念
 
 ## 安装
 
--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@
  * @Author: dushuai
  * @Date: 2024-04-07 11:36:37
  * @LastEditors: dushuai
- * @LastEditTime: 2024-04-09 21:12:55
+ * @LastEditTime: 2024-04-12 18:32:33
  * @description: App 路由 鉴权组件
  */
-import { RouterProvider } from "react-router-dom";
+import { RouterProvider, useLocation } from "react-router-dom";
 import router from "./router";
-import { Suspense, useEffect, useState } from "react";
+import { Suspense, useCallback, useEffect, useState } from "react";
 import Loading from "@/components/Loading";
 import { appStore, permisActions } from "./store";
 import { useSnapshot } from "valtio";
@@ -18,6 +18,8 @@ export default function App() {
 
   const [visible, setVisible] = useState(false)
 
+  // const [location] = useState(useLocation())
+
   useEffect(() => {
     if (token) {
       permisActions.GenerateRoutes()
@@ -26,6 +28,16 @@ export default function App() {
         })
     }
   }, [token])
+
+  const getPath = useCallback(() => {
+    return router.state.location.pathname
+  }, [])
+
+  useEffect(() => {
+    // console.log(router.state.location.pathname);
+    // console.log(getPath());
+
+  }, [getPath()])
 
   /**
    * 当动态路由添加完成 再渲染页面
