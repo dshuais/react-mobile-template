98bd4dc2e5911574ffcf4f116d904dd7fbff7a92
dushuais <1137896420@qq.com>
2024-04-13 20:43:53 +0800

    ✨ feat: 新增loadingtwo和重写弹窗样式

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -2,12 +2,19 @@
  * @Author: dushuai
  * @Date: 2024-03-29 18:14:56
  * @LastEditors: dushuai
- * @LastEditTime: 2024-04-13 17:29:30
+ * @LastEditTime: 2024-04-13 20:32:10
  * @description: loading 组件
  */
-import styles from "./index.module.css"
+import styles from "./index.module.less"
 
 export default function Loading() {
+  return (
+    <LoadingTwo />
+  )
+}
+
+// 规则循环旋转
+function LoadingOne() {
   return (
     <div className={styles.loading}>
       <div className={styles.loader}>
@@ -16,6 +23,15 @@ export default function Loading() {
         <div className={styles.circle}></div>
         <div className={styles.circle}></div>
       </div>
+    </div>
+  )
+}
+
+// 不规则旋转
+function LoadingTwo() {
+  return (
+    <div className={styles['loading-two']}>
+      <div className={styles.loader}></div>
     </div>
   )
 }
